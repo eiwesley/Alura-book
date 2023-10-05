bd9cfcb48bb63d870745e8ed026a119fec83f780
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-05 19:23:07 -0300

    :sparkles: feat: 3 Then e JSON

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/38405364/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/38405364/json/').then(resposta => resposta.json()).then(r => console.log(r));
 
 console.log(consultaCEP);
