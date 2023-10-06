6e1b652e1b53039159bceb5b04e84f2f054f53c6
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 11:19:09 -0300

    :sparkles: feat: 6 Finally

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro){
-        throw Error('Esse CEP não existe')
-    } else {
-        console.log(r)
-    }
-    console.log(r)})
-.catch(erro => console.log(erro));
+var consultaCEP = fetch('https://viacep.com.br/ws/38405364/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse CEP não existe')
+        } else 
+            console.log(r)
+        
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento Concluído'));
 
 console.log(consultaCEP);
