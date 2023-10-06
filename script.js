999420975bd5c521528d16f70edd87d4cf511fa5
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 11:13:10 -0300

    :sparkles: feat: 5 Tratando erros com catch

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/38405364/json/').then(resposta => resposta.json()).then(r => console.log(r));
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if (r.erro){
+        throw Error('Esse CEP não existe')
+    } else {
+        console.log(r)
+    }
+    console.log(r)})
+.catch(erro => console.log(erro));
 
 console.log(consultaCEP);
