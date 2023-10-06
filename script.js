25550fa636670d5e6f49c327c1edb7aaf1ebc260
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 11:29:32 -0300

    :sparkles: feat: 3 Async Await

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/38405364/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse CEP não existe')
-        } else 
-            console.log(r)
-        
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento Concluído'));
+async function buscaEndereco() {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/38405364/json/')
+    var consultaCEPConvertida = await consultaCEP.json()
+    console.log(consultaCEPConvertida);
 
-console.log(consultaCEP);
+}
+
+buscaEndereco()
