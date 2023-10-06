44a86e0f299e48c9256bb72d7c11c0b2488aa94b
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 12:00:24 -0300

    :sparkles: feat: 5 Evitando problemas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
     try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     var consultaCEPConvertida = await consultaCEP.json()
@@ -16,7 +18,7 @@ async function buscaEndereco(cep) {
     console.log(consultaCEPConvertida);
     return consultaCEPConvertida;
     } catch (erro) {
-        console.log(erro)
+        mensagemErro.innerHTML = `<p>CEP Invalido, tente novamente!</p>`
     }
 }
 
