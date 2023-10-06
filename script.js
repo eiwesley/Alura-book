3abb77d9b0cd2a26b169714f13de9e13da7f1acf
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 11:34:15 -0300

    :sparkles: feat: 5 Tratamento de erros

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 async function buscaEndereco() {
-    var consultaCEP = await fetch('https://viacep.com.br/ws/38405364/json/')
+    try {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
     var consultaCEPConvertida = await consultaCEP.json()
+    if (consultaCEPConvertida.erro) {
+        throw Error('CEP não existente')
+    }
     console.log(consultaCEPConvertida);
-
+    } catch (erro) {
+        console.log(erro)
+    }
 }
 
 buscaEndereco()
